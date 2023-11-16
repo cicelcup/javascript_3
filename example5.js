1da34881ba899dbecaef4c272048176c4f5d0cb6
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-16 17:45:50 -0600

    more example

--- example5.js
+++ example5.js
@@ -2,17 +2,17 @@ function newPrinter(value){
     console.log(value)
 }
 
-function getData(){
+function getData(callback){
     console.log("Voy a esperar un segundo")
     setTimeout(() => {
         const sucess = Math.random() > 0.5
         if(sucess){
-            newPrinter('Success')
+            callback('Success')
         }
         else{
-            newPrinter('Error')
+            callback('Error')
         }
     }, 1000)
 }
 
-getData()
+getData(newPrinter)
